56116b57d086a2ee4625e7cb8c48a0f3f4967355
Yrysgul Ergesh <yrysgulergesh@gmail.com>
2022-07-12 13:18:15 +0600

    add scope pytest

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,11 +1,8 @@
 import pytest
 
-@pytest.fixture()
-def set_up():
-    print('Вход в систему выполнен')
 
-def test_sending_mail_1(set_up):
+def test_sending_mail_3(set_up, some):
     print('Письмо отправлено')
 
-def test_sending_mail_2(set_up):
+def test_sending_mail_4(set_up, some):
     print('Письмо отправлено')
